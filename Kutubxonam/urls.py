86236222a74684ef523ec4414a68d035ef5c4b8d
Akmaljon Fayzullayev <akmaljonfayzullayev07@gmail.com>
2023-04-25 21:16:53 +0500

    yangilandi

--- Kutubxonam/urls.py
+++ Kutubxonam/urls.py
@@ -5,7 +5,8 @@ from rest_framework.routers import DefaultRouter
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -30,4 +31,4 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('docs2/', schema_view.with_ui('redoc', cache_timeout=0)),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
